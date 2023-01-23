83460d738437a37f73c15dc9f6bdb46857e58d42
Colby berger <colbyx20@gmail.com>
2023-01-22 20:10:04 -0500

    Prisma Project

--- script.ts
+++ script.ts
@@ -1,46 +1,97 @@
-const { PrismaClient } = require('@prisma/client')
-
+const express = require('express');
+const { urlencoded } = require('express');
+const dotenv = require('dotenv').config();
+const { PrismaClient } = require('@prisma/client');
+const port = process.env.PORT || 5001;
 const prisma = new PrismaClient()
+const app = express();
 
-async function main() {
-  // ... you will write your Prisma Client queries here
+app.use(express.json());
+app.use(urlencoded({extended:false}));
 
-    await prisma.user.create({
+app.get('/api/user', async (req: any,res: any) =>{
+  const users = await prisma.user.findMany();
+  res.json(users);
+  console.dir(users,{depth:null})
+});
+
+app.get('/api/allUsers', async (req:any, res:any) =>{
+
+    const allUsers = await prisma.user.findMany({
+      include:{
+        posts:true,
+        profile:true
+      }
+    });
+    res.status(200).json(allUsers);
+
+    console.dir(allUsers,{depth:null})
+});
+
+app.post('/api/user', async (req: any,res: any) => {
+  const {name,email} = req.body;
+  console.log(name + " " + email);
+  const addNewUser = await prisma.user.create({
         data:{
-            name:'Bob',
-            email:'Bob@knights.ucf.edu'
+            name: name,
+            email: email
         },
-    })
-
-    // const users = await prisma.user.findMany();
-    // console.dir(users,{depth:null})
-
-    // const view = await prisma.user.findMany({
-    //     include:{
-    //         posts: true,
-    //         profile:true,
-    //     },
-    // })
-
-    //  console.dir(view,{depth:null})
-
-    const usersWithPosts = await prisma.user.findMany({
-        include:{
-            posts:true,
-        },
-    })
-
-    console.dir(usersWithPosts,{depth:null})
+    });
+  res.status(200).json(addNewUser);
+});
 
 
-}
+// broken for somereason...
+app.put('/api/updateUser/:id', async (req :any,res:any) =>{
+  const {id} = req.params;
+  const {name} = req.body;
+  console.log(`${id} ${name}`);
+  const post = await prisma.user.update({
+    where:{id},
+    data:{
+      name:name,
+    }
+  });
+  res.json(post);
+});
 
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+app.listen(port,() =>{
+  console.log(`Server starting on port ${port}`);
+});
+
+
+
+
+
+
+
+// async function main() {
+//   // ... you will write your Prisma Client queries here
+
+//     // await prisma.user.create({
+//     //     data:{
+//     //         name:'Bob',
+//     //         email:'Bob@knights.ucf.edu'
+//     //     },
+//     // })
+
+//     const users = await prisma.user.findMany();
+//     console.dir(users,{depth:null})
+
+//     const view = await prisma.user.findMany({
+//         include:{
+
+//             posts: true,
+//             profile:true,
+//         },
+//     })
+
+//     // const usersWithPosts = await prisma.user.findMany({
+//     //     include:{
+//     //         posts:true,
+//     //     },
+//     // })
+
+//     // console.dir(usersWithPosts,{depth:null})
+
+// }
