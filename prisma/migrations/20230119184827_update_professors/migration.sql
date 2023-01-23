4ea5088c7a706bdfdd185434ac491f8b3f69ff75
Colby <colbyx20@gmail.com>
2023-01-22 23:28:08 -0500

    restart

--- prisma/migrations/20230119184827_update_professors/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `password` to the `Professors` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Professors" ADD COLUMN     "password" TEXT NOT NULL;
